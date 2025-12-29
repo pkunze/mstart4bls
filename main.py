8d548d8af209557927fff6133e89d72bd67cf30e
Paul Kunze <paul.kunze@materna.group>
2025-12-29 21:17:06 +0100

    emphasise category

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ if st.radio("Atmung vorhanden?", options=["Ja", "Nein"], index=None) == "Nein":
         st.stop()
 
 if st.radio("Abnormale Atemfrequenz? (Mehr als 5 oder weniger als 2 Atemzüge innerhalb von 10 Sekunden)", options=["Ja", "Nein"], index=None) == "Ja":
-    st.error("Rot &rarr; Stabile Seitenlage")
+    st.error("**ROT** &rarr; Stabile Seitenlage")
     st.link_button("Neuer Patient", "/", use_container_width=True)
     st.stop()
 
 if st.radio ("Spritzende Blutung vorhanden?", options=["Ja", "Nein"], index=None) == "Ja":
-    st.error("Rot &rarr; wenn schnell möglich Druckverband/Tourniquet anlegen, ggfs. Stabile Seitenlage")
+    st.error("**ROT** &rarr; wenn schnell möglich Druckverband/Tourniquet anlegen, ggfs. Stabile Seitenlage")
     st.link_button("Neuer Patient", "/", use_container_width=True)
     st.stop()
 
 last_check = st.radio("**Unfähig** einfache Aufforderungen zu befolgen? (Bspw. \"Folgen Sie mit den Augen meiner Handbewegung!\")", options=["Ja", "Nein"], index=None)
 if last_check == "Ja":
-    st.error("Rot &rarr; Stabile Seitenlage")
+    st.error("**ROT** &rarr; Stabile Seitenlage")
     st.link_button("Neuer Patient", "/", use_container_width=True)
 elif last_check == "Nein":
-    st.warning("Gelb")
+    st.warning("**GELB**")
     st.link_button("Neuer Patient", "/", use_container_width=True)
